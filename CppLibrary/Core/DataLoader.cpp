202af97f7a41cd20d4c3b03cb344b3384864453b
kwon <wingseter@gmail.com>
2024-07-11 14:31:25 +0900

    use precompiled header

---
+++ CppLibrary/Core/DataLoader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 권용훈 on 7/11/24.
+//
+
+#include "DataLoader.h"
---
+++ CppLibrary/Core/DataLoader.h
@@ -0,0 +1,14 @@
+//
+// Created by 권용훈 on 7/11/24.
+//
+
+#ifndef AIRUNNER_DATALOADER_H
+#define AIRUNNER_DATALOADER_H
+
+
+class DataLoader {
+
+};
+
+
+#endif //AIRUNNER_DATALOADER_H
