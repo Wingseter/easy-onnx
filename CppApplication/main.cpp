a8d1ae1e9543a953e25099327a4a4bc95221b1c4
kwon <wingseter@gmail.com>
2024-08-02 02:20:09 +0900

    [test] model loading test

--- CppApplication/main.cpp
+++ CppApplication/main.cpp
@@ -2,10 +2,14 @@
 #include "aiRunner.h"
 
 int main() {
-    const char* modelPath = ""
     std::cout << "Hello This is ai Running Tester" << std::endl;
+    bool cpu_use = true;
+    const char* modelPath = "/Users/kwon/Workspace/C/ai-run-library/SampleModel/best_metric_model_0.7747.onnx";
 
-
-
+    // model initialize
+    if (!InitModel(modelPath, cpu_use)) {
+        std::cerr << "Failed to initialize model." << std::endl;
+        return -1;
+    }
     return 0;
 }
