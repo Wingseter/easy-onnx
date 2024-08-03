b09dcffd2d9d4bc14c7ba93ab39597772143ce02
kwon <wingseter@gmail.com>
2024-08-03 23:09:07 +0900

    [test] test with random data

--- CppApplication/main.cpp
+++ CppApplication/main.cpp
@@ -12,6 +12,26 @@ int main() {
         return -1;
     }
 
+    int dimensions[] = {1, 4, 128, 128, 80};
+    int total_elements = 1;
+
+    // Calculate the total number of elements
+    for (int i = 0; i < 5; ++i) {
+        total_elements *= dimensions[i];
+    }
+
+    // Create a vector to hold the flattened data
+    std::vector<int> data(total_elements);
+
+    // Seed the random number generator
+    std::srand(static_cast<unsigned int>(std::time(0)));
+
+    // Fill the data vector with random values
+    for (int i = 0; i < total_elements; ++i) {
+        data[i] = std::rand() % 100;  // Random values between 0 and 99
+    }
+
+    RunModelInt(data, total_elements);
 
     return 0;
 }
